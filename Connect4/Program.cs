6eca1e106eeae682256c6fd40dfe81ef462be6bc
zaddy6 <isaac.david57@gmail.com>
2019-10-08 15:00:44 +0300

    Designed the menu system

--- Connect4/Program.cs
+++ Connect4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using MenuSystem;
 
 namespace Connect4
 {
@@ -6,7 +8,50 @@ namespace Connect4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var menu = new Menu(1)
+            {
+                Title = "Connect 4",
+                MenuItemsDictionary = new Dictionary<string, MenuItem>()
+                {
+                    {
+                        "1", new MenuItem()
+                        {
+                            Title = "Player Against Computer",
+                            CommandToExecute = null
+                        }
+                    },
+                    {
+                        "2", new MenuItem()
+                        {
+                            Title = "Human Against Human",
+                            CommandToExecute = null
+                        }
+                    }
+                }
+
+            };
+
+            var menu0 = new Menu()
+            {
+                Title = "Play Connect 4",
+                MenuItemsDictionary = new Dictionary<string, MenuItem>()
+                {
+                    {
+                        "S", new MenuItem()
+                        {
+                            Title = "Start Game",
+                            CommandToExecute = menu.Run
+                        }
+                    }
+                    
+                    
+                }
+            };
+
+            menu0.Run();
+
+
+
         }
     }
 }
---
+++ MenuSystem/Menu.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+
+namespace MenuSystem
+{
+    public class Menu
+    {
+        private int _menulevel;
+        private const string MenuCommandExit = "X";
+        private const string ReturnToPrevious = "P";
+        private const string ReturnToMain = "M";
+        
+        private Dictionary<string, MenuItem> _menuItemsDictionary = new Dictionary<string, MenuItem>();
+        
+        
+
+        public Menu(int menulevel = 0)
+        {
+            _menulevel = menulevel;
+        }
+        
+        public string Title { get; set; }
+        
+
+        public Dictionary<string, MenuItem> MenuItemsDictionary
+        {
+            get => _menuItemsDictionary;
+            set
+            {
+                _menuItemsDictionary = value;
+               
+                    if (_menulevel >= 2)
+                    {
+                        _menuItemsDictionary.Add(ReturnToPrevious, new MenuItem(){Title = "Return to Previous Menu"}); 
+                    }
+
+                    if (_menulevel >= 1)
+                    {
+                        _menuItemsDictionary.Add(ReturnToMain, new MenuItem(){Title = "Return to Main Menu", CommandToExecute = Run});
+                    }
+                    _menuItemsDictionary.Add(MenuCommandExit, new MenuItem(){Title = "Exit"});
+                
+            } 
+
+        }
+
+
+
+
+        public string Run()
+        {
+            var command = "";
+            do
+            { 
+                
+                Console.WriteLine(Title);
+                Console.WriteLine("========================");
+            
+                foreach (var item in MenuItemsDictionary)
+                { 
+                    Console.Write(item.Key); 
+                    Console.Write(" "); 
+                    Console.WriteLine(item.Value);
+                    
+                }
+                Console.WriteLine("----------");
+                Console.Write(">");
+
+                command = Console.ReadLine().Trim().ToUpper();
+
+
+
+                var rCommand = "";
+
+                if (MenuItemsDictionary.ContainsKey(command))
+                {
+                    var mItem = MenuItemsDictionary[command];
+                    if (mItem.CommandToExecute != null)
+                    { 
+                        mItem.CommandToExecute();
+                        break;
+                        
+                    }
+
+                    if (command == MenuCommandExit)
+                    {
+                        command = MenuCommandExit;
+                    }
+
+                    if (command == ReturnToMain)
+                    {
+                        _menulevel = 0;
+                    }
+                    
+                    
+                }
+//                else
+//                {
+//                    throw new ArgumentException ("Invalid Command");
+//                }
+            } while (command != MenuCommandExit &&
+                     command != ReturnToMain &&
+                     command != ReturnToPrevious);
+
+            return command;
+        }
+
+
+    }
+}
